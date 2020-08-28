480c5b107dfe49af5555f5c340959a45bdb161fb
Lady Stock <nikki.castlea@gmail.com>
2020-08-28 17:51:11 -0600

    Updates

---
+++ precache-manifest.d09e34056d1be57714a36b7fb50dc170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec62ad0bc4aeb265d3b7fc9c02dbb4f",
+    "url": "/is-it-fall/index.html/index.html"
+  },
+  {
+    "revision": "b641b0590b55b81c5273",
+    "url": "/is-it-fall/index.html/static/js/2.9205012a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/is-it-fall/index.html/static/js/2.9205012a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b7347dd8f9a3cbf005",
+    "url": "/is-it-fall/index.html/static/js/main.1ca35288.chunk.js"
+  },
+  {
+    "revision": "13f8b1fe29b3e396efb7",
+    "url": "/is-it-fall/index.html/static/js/runtime-main.791328c2.js"
+  }
+]);
